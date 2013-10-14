2a104a1b71ec2984465f28e752fe09ebad328c3a
mrodrigues <mrodrigues.uff@gmail.com>
2013-10-14 03:44:45 -0300

    Creating Answer views

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
   config.include(LoginMacros)
 
   config.filter_run wip: true
+  config.run_all_when_everything_filtered = true
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
