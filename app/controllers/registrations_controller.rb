c849662b30c8810f34b65e4b6d16be4b0d20416e
mrodrigues <mrodrigues.uff@gmail.com>
2013-10-13 23:13:27 -0300

    Authentication on all pages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :update_sanitized_params, if: :devise_controller?
+  skip_before_action :authenticate_user!
+  before_action :update_sanitized_params, if: :devise_controller?
+
 
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
